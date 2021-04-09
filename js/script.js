b45c84965b529483a7c6c7f94203983c99306e28
RISMOO <maurice.sider29@gmail.com>
2021-04-09 08:48:05 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function () {
+    setTimeout(function () {
+        
+        $("#message1").toggle("slow");
+    }, 2000);
+
+});
+$(document).ready(function () {
+    setTimeout(function () {
+        
+        $("#ul").toggle("slow");
+    }, 3000);
+
+});
